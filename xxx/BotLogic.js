32cf31a5b05d64cc5d6982aa5813c0e3576b88e5
bartsimpson999 <129171791+bartsimpson999@users.noreply.github.com>
2023-09-29 23:54:59 +0200

    Update BotLogic.js

--- xxx/BotLogic.js
+++ xxx/BotLogic.js
@@ -1,1 +1,55 @@
+// botLogic.js
+
+function Bot(snake, enemy, apple) {
+    this.snake = snake;
+    this.enemy = enemy;
+    this.apple = apple;
+}
+
+Bot.prototype.isEnemyNearby = function() {
+    var myHead = this.snake.body[0];
+    for (var i = 0; i < this.enemy.body.length; i++) {
+        if (Math.abs(myHead.x - this.enemy.body[i].x) <= 1 && Math.abs(myHead.y - this.enemy.body[i].y) <= 1) {
+            return true;
+        }
+    }
+    return false;
+}
+
+Bot.prototype.isSelfCollisionRisk = function() {
+    var myHead = this.snake.body[0];
+    for (var i = 1; i < this.snake.body.length; i++) {
+        if (myHead.x === this.snake.body[i].x && myHead.y === this.snake.body[i].y) {
+            return true;
+        }
+    }
+    return false;
+}
+
+Bot.prototype.decideMove = function() {
+    var myHead = this.snake.body[0];
+    var enemyHead = this.enemy.body[0];
+    var applePosition = this.apple.position;
+
+    // Logic to decide the move based on the positions of the apple, enemy, and potential threats
+    // This is a basic example and can be expanded upon
+    if (!this.isEnemyNearby() && !this.isSelfCollisionRisk()) {
+        if (applePosition.x > myHead.x) {
+            return "RIGHT";
+        } else if (applePosition.x < myHead.x) {
+            return "LEFT";
+        } else if (applePosition.y > myHead.y) {
+            return "DOWN";
+        } else {
+            return "UP";
+        }
+    } else {
+        // Add more logic here for other scenarios
+    }
+}
+
+// Usage in the game loop (in controller.js or snake.js):
+// var bot = new Bot(playerSnake, enemySnake, apple);
+// var move = bot.decideMove();
+// playerSnake.turn(move);
 
