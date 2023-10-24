56a0b7deb2043ce3b631518524bda7a98c781133
bartsimpson999 <129171791+bartsimpson999@users.noreply.github.com>
2023-10-24 19:03:01 +0200

    Update bot.js

--- xxx/bot.js
+++ xxx/bot.js
@@ -1,36 +1,69 @@
 function determineBotMove(game) {
-  // Ottieni la posizione della testa del nostro serpente e del serpente nemico
-  const ourSnakeHead = game.snakes[1].body[0];
-  const enemySnakeHead = game.snakes[0].body[0];
-  const enemySnakeTail = game.snakes[0].body[game.snakes[0].body.length - 1];
-  const food = game.food[0];
+  var mySnake = game.snakes[0];
+  var botSnake = game.snakes[1];
+  var food = game.food[0];
 
-  // Calcola la distanza tra la nostra testa e altri oggetti di interesse
-  const distanceToEnemyHead = calculateDistance(ourSnakeHead, enemySnakeHead);
-  const distanceToEnemyTail = calculateDistance(ourSnakeHead, enemySnakeTail);
-  const distanceToFood = calculateDistance(ourSnakeHead, food);
+  var directions = ["north", "south", "east", "west"];
+  var bestDirection = null;
+  var bestScore = -Infinity;
 
-  // Se la mela è la più vicina, dirigiti verso di essa
-  if (distanceToFood < distanceToEnemyHead && distanceToFood < distanceToEnemyTail) {
-    return getDirectionTowards(ourSnakeHead, food);
-  }
+  directions.forEach(function(direction) {
+    var score = evaluateMove(direction, game);
+    if (score > bestScore) {
+      bestScore = score;
+      bestDirection = direction;
+    }
+  });
 
-  // Se la coda del nemico è la più vicina e accessibile, dirigiti verso di essa
-  if (distanceToEnemyTail < distanceToEnemyHead) {
-    return getDirectionTowards(ourSnakeHead, enemySnakeTail);
-  }
-
-  // Altrimenti, dirigiti verso la testa del nemico
-  return getDirectionTowards(ourSnakeHead, enemySnakeHead);
+  return bestDirection;
 }
 
-function calculateDistance(coord1, coord2) {
-  return Math.abs(coord1[0] - coord2[0]) + Math.abs(coord1[1] - coord2[1]);
+function evaluateMove(direction, game) {
+  var mySnake = game.snakes[0];
+  var botSnake = game.snakes[1];
+  var food = game.food[0];
+
+  // Simula il movimento del bot in una certa direzione
+  var newPos = simulateMove(botSnake.body[0], direction);
+
+  // Controlla se la nuova posizione causerà una collisione
+  if (isCollision(newPos, mySnake) || isCollision(newPos, botSnake)) {
+    return -Infinity; // mossa non valida
+  }
+
+  var score = 0;
+
+  // Aumenta il punteggio se la mossa porta il bot più vicino al cibo
+  var distanceToFood = Math.abs(newPos[0] - food[0]) + Math.abs(newPos[1] - food[1]);
+  score -= distanceToFood;
+
+  // Aumenta il punteggio se la mossa porta il bot a prendere la coda del giocatore
+  var distanceToTail = Math.abs(newPos[0] - mySnake.body[mySnake.body.length - 1][0]) + Math.abs(newPos[1] - mySnake.body[mySnake.body.length - 1][1]);
+  score -= distanceToTail * 0.5; // la coda è meno prioritaria rispetto al cibo
+
+  // Aumenta il punteggio se la mossa porta il bot a colpire la testa del giocatore
+  var distanceToHead = Math.abs(newPos[0] - mySnake.body[0][0]) + Math.abs(newPos[1] - mySnake.body[0][1]);
+  if (distanceToHead == 1) {
+    score += 100; // colpire la testa è molto vantaggioso
+  }
+
+  return score;
 }
 
-function getDirectionTowards(from, to) {
-  if (from[0] < to[0]) return "east";
-  if (from[0] > to[0]) return "west";
-  if (from[1] < to[1]) return "south";
-  return "north";
+function simulateMove(pos, direction) {
+  var newPos = pos.slice();
+  if (direction == "north") newPos[1]--;
+  if (direction == "south") newPos[1]++;
+  if (direction == "east") newPos[0]++;
+  if (direction == "west") newPos[0]--;
+  return newPos;
+}
+
+function isCollision(pos, snake) {
+  for (var i = 0; i < snake.body.length; i++) {
+    if (pos[0] == snake.body[i][0] && pos[1] == snake.body[i][1]) {
+      return true;
+    }
+  }
+  return false;
 }
