7b6d682065b2b09edc063ecd74edec9b71eb4c8a
bartsimpson999 <129171791+bartsimpson999@users.noreply.github.com>
2023-10-24 19:15:51 +0200

    Update bot.js

--- xxx/bot.js
+++ xxx/bot.js
@@ -6,84 +6,81 @@ function determineBotMove(game) {
   var gameWidth = game.width; // Assumendo che il gioco abbia una proprietà width
   var gameHeight = game.height; // Assumendo che il gioco abbia una proprietà height
 
-  var obstacles = mySnake.body.concat(botSnake.body);
-  var pathToFood = aStarSearch(botSnake.body[0], food, gameWidth, gameHeight, obstacles);
-  var pathToTail = aStarSearch(botSnake.body[0], mySnake.body[mySnake.body.length - 1], gameWidth, gameHeight, obstacles);
+  var directions = ["north", "south", "east", "west"];
+  var bestDirection = null;
+  var bestScore = -Infinity;
 
-  // Valuta i percorsi e scegli la mossa migliore
-  // ... (resto del codice per valutare i percorsi e scegliere la mossa migliore)
-
-  return bestDirection; // Restituisci la direzione scelta come migliore
-}
-
-function getNeighbors(pos, gameWidth, gameHeight) {
-  var neighbors = [];
-  var directions = [
-    [0, -1], // north
-    [0, 1],  // south
-    [1, 0],  // east
-    [-1, 0]  // west
-  ];
-  for (var i = 0; i < directions.length; i++) {
-    var x = pos[0] + directions[i][0];
-    var y = pos[1] + directions[i][1];
-    if (x < 0) x = gameWidth - 1;
-    if (x >= gameWidth) x = 0;
-    if (y < 0) y = gameHeight - 1;
-    if (y >= gameHeight) y = 0;
-    neighbors.push([x, y]);
-  }
-  return neighbors;
-}
-
-function heuristic(pos, goal) {
-  var dx = Math.abs(pos[0] - goal[0]);
-  var dy = Math.abs(pos[1] - goal[1]);
-  return dx + dy;
-}
-
-function aStarSearch(start, goal, gameWidth, gameHeight, obstacles) {
-  var openSet = [start];
-  var cameFrom = {};
-  var gScore = {};
-  var fScore = {};
-  gScore[start] = 0;
-  fScore[start] = heuristic(start, goal);
-
-  while (openSet.length > 0) {
-    var current = openSet.reduce((a, b) => fScore[a] < fScore[b] ? a : b);
-    if (current[0] == goal[0] && current[1] == goal[1]) {
-      return reconstructPath(cameFrom, current);
+  directions.forEach(function(direction) {
+    var score = evaluateMove(direction, game, gameWidth, gameHeight);
+    if (score > bestScore) {
+      bestScore = score;
+      bestDirection = direction;
     }
+  });
 
-    openSet = openSet.filter(cell => cell[0] != current[0] || cell[1] != current[1]);
-    var neighbors = getNeighbors(current, gameWidth, gameHeight);
+  return bestDirection;
+}
 
-    for (var i = 0; i < neighbors.length; i++) {
-      var neighbor = neighbors[i];
-      if (obstacles.some(obstacle => obstacle[0] == neighbor[0] && obstacle[1] == neighbor[1])) {
-        continue;
-      }
-      var tentativeGScore = gScore[current] + 1;
-      if (!gScore[neighbor] || tentativeGScore < gScore[neighbor]) {
-        cameFrom[neighbor] = current;
-        gScore[neighbor] = tentativeGScore;
-        fScore[neighbor] = gScore[neighbor] + heuristic(neighbor, goal);
-        if (!openSet.some(cell => cell[0] == neighbor[0] && cell[1] == neighbor[1])) {
-          openSet.push(neighbor);
-        }
-      }
+function evaluateMove(direction, game, gameWidth, gameHeight) {
+  var mySnake = game.snakes[0];
+  var botSnake = game.snakes[1];
+  var food = game.food[0];
+
+  // Simula il movimento del bot in una certa direzione
+  var newPos = simulateMove(botSnake.body[0], direction, gameWidth, gameHeight);
+
+  // Controlla se la nuova posizione causerà una collisione
+  if (isCollision(newPos, mySnake) || isCollision(newPos, botSnake)) {
+    return -Infinity; // mossa non valida
+  }
+
+  var score = 0;
+
+  // Aumenta il punteggio se la mossa porta il bot più vicino al cibo
+  var distanceToFood = Math.abs(newPos[0] - food[0]) + Math.abs(newPos[1] - food[1]);
+  score -= distanceToFood;
+
+  // Aumenta il punteggio se la mossa porta il bot a prendere la coda del giocatore
+  var distanceToTail = Math.abs(newPos[0] - mySnake.body[mySnake.body.length - 1][0]) + Math.abs(newPos[1] - mySnake.body[mySnake.body.length - 1][1]);
+  score -= distanceToTail * 0.5; // la coda è meno prioritaria rispetto al cibo
+
+  // Aumenta il punteggio se la mossa porta il bot a colpire la testa del giocatore
+  var distanceToHead = Math.abs(newPos[0] - mySnake.body[0][0]) + Math.abs(newPos[1] - mySnake.body[0][1]);
+  if (distanceToHead == 1) {
+    score += 100; // colpire la testa è molto vantaggioso
+  }
+
+  return score;
+}
+
+function simulateMove(pos, direction, gameWidth, gameHeight) {
+  var newPos = pos.slice();
+  if (direction == "north") {
+    newPos[1]--;
+    if (newPos[1] < 0) newPos[1] = gameHeight - 1; // Se supera il bordo superiore, riappare in basso
+  }
+  if (direction == "south") {
+    newPos[1]++;
+    if (newPos[1] >= gameHeight) newPos[1] = 0; // Se supera il bordo inferiore, riappare in alto
+  }
+  if (direction == "east") {
+    newPos[0]++;
+    if (newPos[0] >= gameWidth) newPos[0] = 0; // Se supera il bordo destro, riappare a sinistra
+  }
+  if (direction == "west") {
+    newPos[0]--;
+    if (newPos[0] < 0) newPos[0] = gameWidth - 1; // Se supera il bordo sinistro, riappare a destra
+  }
+  return newPos;
+}
+
+function isCollision(pos, snake) {
+  for (var i = 0; i < snake.body.length; i++) {
+    if (pos[0] == snake.body[i][0] && pos[1] == snake.body[i][1]) {
+      return true;
     }
   }
-  return null;
+  return false;
 }
 
-function reconstructPath(cameFrom, current) {
-  var totalPath = [current];
-  while (cameFrom[current]) {
-    current = cameFrom[current];
-    totalPath.unshift(current);
-  }
-  return totalPath;
-}
 
