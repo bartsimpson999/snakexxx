1a527b1dd3eb3c8cedc03aff34bc10e967b78552
bartsimpson999 <129171791+bartsimpson999@users.noreply.github.com>
2023-10-24 20:01:49 +0200

    Update bot.js

--- xxx/bot.js
+++ xxx/bot.js
@@ -1,8 +1,4 @@
 function determineBotMove(game) {
-  var mySnake = game.snakes[0];
-  var botSnake = game.snakes[1];
-  var food = game.food[0];
-
   var directions = ["north", "south", "east", "west"];
   var bestDirection = null;
   var bestScore = -Infinity;
@@ -23,55 +19,43 @@ function evaluateMove(direction, game) {
   var botSnake = game.snakes[1];
   var food = game.food[0];
 
-  // Simula il movimento del bot in una certa direzione
   var newPos = simulateMove(botSnake.body[0], direction, game.width, game.height);
 
-  // Controlla se la nuova posizione causerà una collisione
-  if (isCollision(newPos, mySnake) || isCollision(newPos, botSnake)) {
-    return -Infinity; // mossa non valida
+  if (isCollision(newPos, mySnake.body) || isCollision(newPos, botSnake.body)) {
+    return -Infinity;
   }
 
   var score = 0;
 
-  // Aumenta il punteggio se la mossa porta il bot più vicino al cibo
-  var distanceToFood = getDistance(newPos, food, game.width, game.height);
-  score -= distanceToFood;
+  score -= getDistance(newPos, food, game.width, game.height);
+  score -= getDistance(newPos, mySnake.body[mySnake.body.length - 1], game.width, game.height) * 0.5;
 
-  // Aumenta il punteggio se la mossa porta il bot a prendere la coda del giocatore
-  var distanceToTail = getDistance(newPos, mySnake.body[mySnake.body.length - 1], game.width, game.height);
-  score -= distanceToTail * 0.5; // la coda è meno prioritaria rispetto al cibo
-
-  // Aumenta il punteggio se la mossa porta il bot a colpire la testa del giocatore
-  var distanceToHead = getDistance(newPos, mySnake.body[0], game.width, game.height);
-  if (distanceToHead == 1) {
-    score += 100; // colpire la testa è molto vantaggioso
+  if (getDistance(newPos, mySnake.body[0], game.width, game.height) == 1) {
+    score += 100;
   }
 
-  // Se il bot sta seguendo la coda del giocatore e il giocatore sta per prendere il cibo, riduci il punteggio
-  if (distanceToTail == 1 && distanceToFood <= 1) {
-    score -= 200; // penalizza fortemente questa mossa
+  if (getDistance(newPos, mySnake.body[mySnake.body.length - 1], game.width, game.height) == 1 && getDistance(mySnake.body[0], food, game.width, game.height) <= 2) {
+    score -= 200;
   }
 
   return score;
 }
 
 function simulateMove(pos, direction, width, height) {
-  var newPos = pos.slice();
-  if (direction == "north") {
-    newPos[1]--;
-    if (newPos[1] < 0) newPos[1] = height - 1;
-  }
-  if (direction == "south") {
-    newPos[1]++;
-    if (newPos[1] >= height) newPos[1] = 0;
-  }
-  if (direction == "east") {
-    newPos[0]++;
-    if (newPos[0] >= width) newPos[0] = 0;
-  }
-  if (direction == "west") {
-    newPos[0]--;
-    if (newPos[0] < 0) newPos[0] = width - 1;
+  var newPos = [...pos];
+  switch (direction) {
+    case "north":
+      newPos[1] = (newPos[1] - 1 + height) % height;
+      break;
+    case "south":
+      newPos[1] = (newPos[1] + 1) % height;
+      break;
+    case "east":
+      newPos[0] = (newPos[0] + 1) % width;
+      break;
+    case "west":
+      newPos[0] = (newPos[0] - 1 + width) % width;
+      break;
   }
   return newPos;
 }
@@ -79,18 +63,15 @@ function simulateMove(pos, direction, width, height) {
 function getDistance(pos1, pos2, width, height) {
   var dx = Math.abs(pos1[0] - pos2[0]);
   var dy = Math.abs(pos1[1] - pos2[1]);
-  // Considera la possibilità di attraversare il bordo
+
   dx = Math.min(dx, width - dx);
   dy = Math.min(dy, height - dy);
+
   return dx + dy;
 }
 
-function isCollision(pos, snake) {
-  for (var i = 0; i < snake.body.length; i++) {
-    if (pos[0] == snake.body[i][0] && pos[1] == snake.body[i][1]) {
-      return true;
-    }
-  }
-  return false;
+function isCollision(pos, body) {
+  return body.some(segment => segment[0] === pos[0] && segment[1] === pos[1]);
 }
+
 
